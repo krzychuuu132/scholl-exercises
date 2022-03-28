5f1128292c35b21e4475d22735e08b8b604a8837
Krzysztof Urban <krzychu222u@wp.pl>
2022-03-28 06:36:46 +0200

    solving tasks

--- src/company/Main.java
+++ src/company/Main.java
@@ -2,11 +2,14 @@ package company;
 
 public class Main {
     public static void main(String[] args){
+
+        // Konstruktory a kompozycja
         Engine silnik = new Engine("sprawny");
         Vehicle audi = new Vehicle("audi","a3",120000,silnik,12000);
         audi.getInfo();
         audi.driveCar();
 
+        // Konstruktory a dziedziczenie
         Car car = new Car("audi","a3",120000,silnik);
         car.driveCar();
 
